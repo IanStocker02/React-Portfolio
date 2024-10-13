085699788f48bfa40392cb94664b1bf4eba4183b
IanStocker02 <iant.stocker@gmail.com>
2024-10-13 14:38:22 -0400

    fixed pathing issues

--- src/app.jsx
+++ src/app.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import './app.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Portfolio from './pages/portfolio';
-import Header from './components/Header';
+import Header from './components/header';
 import Contact from './pages/contact';
 import Resume from './pages/resume';
 import AboutMe from './pages/aboutme';
-import Footer from './components/Footer'; // Ensure Footer is correctly imported
+import Footer from './components/footer'; // Ensure Footer is correctly imported
 
 function App() {
   return (
