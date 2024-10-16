19c60459830fce2b835af11b65c36e3c7f121ca0
IanStocker02 <iant.stocker@gmail.com>
2024-10-15 20:19:24 -0400

    Fix white page issue

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: '/src/main.jsx', // Update this line
-    },
-  },
+  base: '/'
 });
